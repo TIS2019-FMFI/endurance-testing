44bba0b6b68b0b088635082c7f5d145c3ad9f759
elias29 <eliasfilip.ef@gmail.com>
2020-01-21 20:48:47 +0100

    sharovany jeden hlavny dataset

--- DatabazaZivotnosti/src/DatabaseConnector/ProduktFinder.java
+++ DatabazaZivotnosti/src/DatabaseConnector/ProduktFinder.java
@@ -12,25 +12,29 @@ import javax.sql.DataSource;
 import DataProcessing.Produkt;
 
 public class ProduktFinder {
-	@Resource(mappedName="java:jboss/datasources/databazazivotnostiDS")
 	private DataSource dataSource;
 	
-	public ProduktFinder() { }
+	public ProduktFinder(DataSource datasource) { 
+		dataSource = datasource;
+	}
 	
-	public List<Produkt> produktFinder(String kunde, String zeichnungsnummer, String bezeichnung){
-		List<Produkt> zoznam = new ArrayList<Produkt>(); 
+	public List<Produkt> produktFinder(String kunde, String zeichnungsnummer, String bezeichnung, String nr){
+		List<Produkt> zoznam = new ArrayList<Produkt>(); 	
 		try {
 			java.sql.Connection c = dataSource.getConnection();
-			PreparedStatement s = c.prepareStatement("Select * from produkt where Kunde = ? AND Zeichnungsnummer = ? and Bezeichnung = ?");
-			s.setString(0, kunde);
-			s.setString(1, zeichnungsnummer);
-			s.setString(2, bezeichnung);
+			PreparedStatement s = c.prepareStatement("Select * from produkt where Kunde = ? AND Zeichnungsnummer LIKE ? and Bezeichnung = ? and NR = ?");
+			s.setString(1, kunde);
+			s.setString(2, zeichnungsnummer);
+			s.setString(3, bezeichnung);
+			s.setString(4, nr);
 			ResultSet rs = s.executeQuery();
 			while (rs.next()) {
-				Produkt p = new Produkt("","","");
+				Produkt p = new Produkt("","","","",dataSource);
 				p.setBezeichnung(rs.getString("Bezeichnung"));
 				p.setKunde(rs.getString("Kunde"));
 				p.setZeichnungsnummer(rs.getString("Zeichnungsnummer"));
+				p.setNr(rs.getString("NR"));
+				p.setId(rs.getInt("Id"));
 				zoznam.add(p); 
 			}
 			return zoznam;
@@ -40,5 +44,24 @@ public class ProduktFinder {
 		}
 		return null;
 	}
-
+	
+	public Integer produktIDFinder(String kunde, String zeichnungsnummer, String bezeichnung, String nr){
+		try {
+			java.sql.Connection c = dataSource.getConnection();
+			PreparedStatement s = c.prepareStatement("Select Id from produkt where Kunde = ? AND Zeichnungsnummer = ? and Bezeichnung = ? and NR = ?");
+			s.setString(1, kunde);
+			s.setString(2, zeichnungsnummer);
+			s.setString(3, bezeichnung);
+			s.setString(4, nr);
+			ResultSet rs = s.executeQuery();
+			while (rs.next()) {
+				return rs.getInt("Id");
+			}
+			return -99;
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
--- DatabazaZivotnosti/src/DatabaseConnector/VerziaInsert.java
+++ DatabazaZivotnosti/src/DatabaseConnector/VerziaInsert.java
@@ -9,10 +9,11 @@ import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 public class VerziaInsert {
-	@Resource(mappedName="java:jboss/datasources/databazazivotnostiDS")
 	private DataSource dataSource;
 	
-	public VerziaInsert() { }
+	public VerziaInsert(DataSource datasource) {
+		dataSource = datasource;
+	}
 	
 	public void insert(String zeichnungsnummer, String verzia, Integer id_objednavky){
 		try(PreparedStatement st = dataSource.getConnection().prepareStatement("Insert into verzia (Zeichnungsnummer,Verzia, Id_objednavky) values (?,?,?)", Statement.RETURN_GENERATED_KEYS)){
